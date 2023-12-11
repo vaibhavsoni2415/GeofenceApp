f47c1eae106d997a2e585523a024fc837393269b
Vaibhav <vaibhavsoni2415@gmail.com>
2023-12-11 19:32:14 +0530

    Adding initial code.

---
+++ GeofenceApp/GeofenceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceApp
+//
+//  Created by Vaibhav on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
